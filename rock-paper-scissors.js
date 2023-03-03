a5aeb31bc07e90ecebeb2c2f730eb40ffcb7ec26
Rafail <123392637+RafailDD@users.noreply.github.com>
2023-03-03 13:33:07 +0200

    Add playRound function with the core logic of the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     let computerChoice=Math.ceil(3*Math.random());
-    //console.log(computerChoice);
     switch (computerChoice) {
         case 1:
             return "rock";
@@ -13,4 +12,31 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "It's a tie!";
+        } else if (computerSelection === "paper") {
+            return "You lose! Paper beats rock";
+        } else if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock";
+        } else if (computerSelection === "paper") {
+            return "It's a tie!";
+        } else if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors";
+        } else if (computerSelection === "paper") {
+            return "You win! Scissors beats paper";
+        } else if (computerSelection === "scissors") {
+            return "It's a tie!";
+        }
+    }
+}
